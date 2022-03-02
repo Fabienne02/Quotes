cf89bab4727ba135d021500a3948e384ccc0850a
Fabienne van den Steen <74105233+Fabienne02@users.noreply.github.com>
2022-03-02 19:01:49 +0100

    Add files via upload

--- docs/script/js.js
+++ docs/script/js.js
@@ -1,7 +1,14 @@
 // To do:
-// - autheur apart
-// - zero/laad stat erin en error beter uitwerken
-// - div eruit 
+// - autheur apart - done
+// - zero/laad state erin - done
+// - error beter uitwerken - done
+// - div eruit - done
+// - Modules 
+// - hash routes voor de focus state
+// - JS opschonen
+
+
+
 
 const app = document.getElementById('root')
 
@@ -12,9 +19,11 @@ request.responseType = "json";
 
 request.onload = function () {
   // Begin accessing JSON data here
-  var info = request.response.data
+  var quote = request.response.data
+  console.log(quote)
   if (request.status >= 200 && request.status < 400) {
-    info.forEach(quote => {
+
+    quote.forEach(quote => {
       const figure = document.createElement('figure')
       figure.setAttribute('class', 'figure')
 
@@ -25,16 +34,58 @@ request.onload = function () {
       quote.name = quote.name.substring(0, 300)
       cite.textContent = `${quote.name}`
 
+      console.log("wie ben ik? ", quote)
+      const focus = document.createElement('a')
+      focus.setAttribute("href", `#${quote.name}`);
+      focus.textContent = `#`
+      var statusquote = false
+      
+
+      focus.onclick = function highlighted(){
+        if ( statusquote == false ) {
+        console.log("welke a? ",quote)
+        figure.classList.add("highlight")
+        figure.id = `${quote.name}`
+        document.querySelector("body").style.overflowY = "hidden"
+
+        statusquote = true 
+      } else if ( statusquote == true ) {
+        figure.removeAttribute("id");
+        figure.classList.remove("highlight")
+        document.querySelector("body").style.overflowY = "scroll"
+        //focus.setAttribute("href", '');
+        focus.removeAttribute("href");
+
+        statusquote = false 
+      }
+      }
+
       app.appendChild(figure)
       figure.appendChild(blockquote)
       figure.appendChild(cite)
+      figure.appendChild(focus)
 
     } )
+
   } else {
-    const errorMessage = document.createElement('marquee')
-    errorMessage.textContent = `Gah, it's not working!`
-    app.appendChild(errorMessage)
+    // Deze error gebeurd pas wanneer request.status <200 && > 400 is
+    const errorMessagestatus = document.createElement('marquee')
+    errorMessagestatus.textContent = `The Server is working hard! please try again later`
+    app.appendChild(errorMessagestatus)
+
+    console.log("ERROR!")
   }
+}
+
+// Deze error gebeurd al wanneer de API niet gelezen kan worden, geen request.status
+// Beide errors houden dezelfde css aan door oproeping van element tags in dezelfde #root AKA const app
+request.onerror = function () {
+  console.log("ERROR")
+
+  const errorMessage = document.createElement('marquee')
+  errorMessage.setAttribute("scrollamount", "10");
+  errorMessage.textContent = `The Server is working hard! please try again later`
+  app.appendChild(errorMessage)
 }
 
 
@@ -46,14 +97,17 @@ request.send()
 const button = document.querySelectorAll(".bottom")
 const inDruk = document.querySelector(".bottom")
 const blockquote = document.createElement('blockquote')
+const cite = document.createElement('cite')
 
 const day = document.getElementById('day')
-
 
 
 button.forEach((inDruk) => {
     inDruk.addEventListener("click", show)
 });
+
+
+
 
 function show() {
     if (request.status >= 200 && request.status < 400) {
@@ -64,23 +118,92 @@ function show() {
 
       const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
       console.log(randomQuote)
-      // om de autheur eronder mischien cite of de p.context te herhalen dus nieuw element
-      blockquote.textContent = `${randomQuote.text} ${randomQuote.name}`
+      blockquote.textContent = `${randomQuote.text}`
+      cite.textContent = `${randomQuote.name}`
+
 
       day.appendChild(blockquote)
-
+      day.appendChild(cite)
+      
+      // Loading manipulation with delay
+      day.classList.remove("display");
+    setTimeout(() => {
+        day.classList.add("display");
+    }, 3000);
 
     } else  {
-    
-      p.textContent = none
-      // statusquote = false
+      //Deze error gebeurd wel omdat het een onclick event is
+      //Hierbij maken we geen nieuw element aan maar gebruiken we de p in de header want anders zou dit elke keer als we onclick uitvoeren gebeuren -> too many errors!
+      console.log("no quote today")
 
-      day.appendChild(p)
-  }
+      const errorMessagestatusquote = document.getElementById("errorquoteoftheday")
+      errorMessagestatusquote.textContent = `Looks like there is no quote today, sorry!`
+      // .appendChild(errorMessagestatusquote)
+      errorquoteoftheday.classList.remove("display");
+      setTimeout(() => {
+        errorquoteoftheday.classList.add("display");
+      }, 3000);
+
+      const errorbutton = document.querySelector("button")
+      errorbutton.style.backgroundColor = "lightgrey"
+      document.querySelector("button").disabled = true;
+    }
     
 
 }
 
 
+// JAVASCRIPT LOADING STATE
+const btn = document.querySelector(".bottom");
+btn.addEventListener("click", displayLoading);
+const loader = document.querySelector("#loading");
+
+function displayLoading() {
+    loader.classList.add("display");
+    // to stop loading after some time
+    setTimeout(() => {
+        loader.classList.remove("display");
+    }, 3000);
+}
+
+// hiding loading 
+function hideLoading() {
+    loader.classList.remove("display");
+}
+
+
+// JAVASCRIPT VOOR DE SINGLEQUOTE HASH ROUTE
+
+// Vraag: Hoe kan ik de # koppelen aan de quote, deze quote full screen krijgen en de styling behouden?
+
+// const focussedapp = document.getElementById('singlequote')
+// const header = document.querySelector('header')
+// const focussedquote = document.getElementsByClassName('highlight')
+// const figures = document.getElementsByClassName('figure')
+
+
+// function onRouteChanged() {
+//   console.log("Hash changed!");
+//   figures.style.display = "none"
+//   header.style.display = "none"
+//   focussedquote.style.display = "block"
+
+
+//       const p = document.createElement('p')
+//       p.textContent = "hi"
+
+//       // Geef # de id van quote
+//       const focus = document.createElement('a')
+//       focus.textContent = `#`
+//       focus.setAttribute("href", "");
+
+
+//       focussedapp.appendChild(p)
+//       focussedapp.appendChild(focus)
+
+
+// }
+
+// window.addEventListener("hashchange", onRouteChanged);
 
 
