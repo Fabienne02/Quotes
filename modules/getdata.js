824ddbf63a72dbe0b5e34b62538864f49d004f29
Fabienne van den Steen <74105233+Fabienne02@users.noreply.github.com>
2022-03-09 11:48:15 +0100

    Add files via upload

---
+++ modules/getdata.js
@@ -0,0 +1,17 @@
+//fetch from api 
+export async function getdata(){
+  const url = 'https://quote.api.fdnd.nl/v1/quote'
+  const data = await fetch(url)
+  const response = await data.json()
+  if(data.status >= 200 && data.status < 400){
+  return response
+  } else {
+    return Promise.reject(data.status)
+  }
+   } 
+  
+
+
+  //  const app = document.getElementById('root')
+
+
